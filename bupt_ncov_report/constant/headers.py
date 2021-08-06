10f0eaec4b58e05399e5a393e9afbcd08859270c
Sansui233 <sansuilnm@gmail.com>
2021-08-07 01:28:33 +0800

    change UA

--- bupt_ncov_report/constant/headers.py
+++ bupt_ncov_report/constant/headers.py
@@ -4,8 +4,8 @@ class HEADERS:
     REFERER_POST_API = 'https://app.bupt.edu.cn/ncov/wap/default/index'
     ORIGIN_BUPTAPP = 'https://app.bupt.edu.cn'
 
-    UA = ('Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
-          'Mobile/15E148 MicroMessenger/7.0.11(0x17000b21) NetType/4G Language/zh_CN')
+    UA = ('Mozilla/5.0 (iPhone; CPU iPhone OS 14_0like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
+          'Mobile/15E148 MicroMessenger/7.0.17(0x17001126) NetType/WIFI Language/zh_CN')
     ACCEPT_JSON = 'application/json'
     ACCEPT_HTML = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
     REQUEST_WITH_XHR = 'XMLHttpRequest'
