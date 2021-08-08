050d82d412ae9c994d07506ddfdfad67be4305ea
Sansui233 <sansuilnm@gmail.com>
2021-08-08 12:26:13 +0800

    typo

--- runCheckin.sh
+++ runCheckin.sh
@@ -9,7 +9,7 @@ rnd=$(rand 1 50)
 echo "Sleep for ${rnd} minutes"
 sleep ${rnd}m
 export BUPT_SSO_USER=
-export export BUPT_SSO_PASS=
+export BUPT_SSO_PASS=
 export SERVER_CHAN_SCKEY=
 basepath=$(cd `dirname $0`; pwd)
 python3 ${basepath}/main.py --stop-when-sick
